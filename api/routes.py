89e665bdada2a262e02f23a20ae6117afa24a947
9Shuck <rubb.phz@gmail.com>
2021-10-01 20:26:07 +0200

    finished all endpoints/REVIEW pending

--- api/routes.py
+++ api/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, json, jsonify, request
 from sqlalchemy import exc
-from api.models import User, Customer
+from sqlalchemy.sql.expression import except_
+from api.models import db, User, Customer, modifications
 from api.validations import Validations
 from api.errors import Errors
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -93,7 +94,7 @@ def post_customer():
     return Errors.name_not_valid()
 
 @api.route('/user/<int:id>', methods=['DELETE'])
-@jwt_required
+@jwt_required()
 def delete_user(id):
     if Validations.is_admin(get_jwt_identity()):
         if Validations.is_active(id):
@@ -104,3 +105,95 @@ def delete_user(id):
             return Errors.user_not_found()    
         return Errors.user_not_found()
     return Errors.not_admin()
+
+@api.route('/customer/<int:id>', methods=['DELETE'])
+@jwt_required()
+def delete_customer(id):
+    if Validations.is_admin(get_jwt_identity()) or Validations.customer_user(id, get_jwt_identity()):
+        if Validations.is_customer_active(id):
+            customer = Customer.get_by_id(id)
+            if customer:
+                customer.disable_user()
+                return jsonify(customer.serialize()), 200
+            return Errors.customer_not_found()    
+        return Errors.customer_not_found()
+    return Errors.not_admin()
+
+@api.route('/user/<int:id>', methods=['PATCH'])
+@jwt_required()
+def update_user(id):
+    if Validations.is_admin(get_jwt_identity()): 
+
+        update_user = {
+                    'email' : request.json.get('email', None),
+                    'password': request.json.get('password', None),
+                    'is_admin': request.json.get('is_admin', None)
+        }
+        
+        if update_user["password"] and Validations.password(update_user['password']):
+            password = generate_password_hash(
+                update_user["password"], method='pbkdf2:sha256', salt_length=16)
+            update_user["password"] = password
+
+        user = User.get_by_id(id)
+
+        if user and Validations.email(update_user['email']):
+            try:
+                updated_user = user.update(**{
+                                    key: value for key, value in update_user.items()
+                                    if value is not None
+                                })
+                return jsonify(updated_user.serialize()), 200
+            except exc.IntegrityError:
+                return Errors.email_already_registered()
+        return Errors.user_not_found()
+    return Errors.not_admin()
+
+@api.route('/customer/<int:id>', methods=['PATCH'])
+@jwt_required()
+def update_customer(id):
+    if Validations.is_admin(get_jwt_identity()) or Validations.customer_user(id, get_jwt_identity()):
+        update_customer = {
+                    'name' : request.json.get('name', None),
+                    'surname': request.json.get('surname', None),
+                    'photo_url': request.json.get('photo_url', None)
+        }
+
+        customer = Customer.get_by_id(id)
+
+        if customer and Validations.customer_name(update_customer['name']) and Validations.customer_name(update_customer['surname']):
+            updated_customer = customer.update(**{
+                                key: value for key, value in update_customer.items()
+                                if value is not None
+                            })
+            
+            try:
+                modification = modifications.insert().values(user_id=get_jwt_identity(), customer_id=customer.id)
+                db.session.execute(modification)
+                db.session.commit()
+            except exc.IntegrityError:
+                pass
+            return jsonify(updated_customer.serialize()), 200
+        return Errors.customer_not_found()
+    return Errors.not_admin()
+
+'''
+From here are just things for testing. The endpoint /registerno allow to create 
+users in the database but has to be removed.
+'''
+
+@api.route('/registerno', methods=['POST'])
+def user_register_no():
+    email = request.json.get('email', None)
+    password = request.json.get('password', None)
+
+    user = User(
+        email = email,
+        is_admin = True,
+        password = generate_password_hash(password, method='pbkdf2:sha256', salt_length=16)
+    )
+    try: 
+        user.create()
+        return jsonify(user.serialize()), 201
+    except exc.IntegrityError:
+        return Errors.email_already_registered()
