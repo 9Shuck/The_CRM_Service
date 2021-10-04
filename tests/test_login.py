7e7548d97404b22dce13548e78e6661c9ef444df
9Shuck <rubb.phz@gmail.com>
2021-10-04 22:03:28 +0200

    tests working but not mock

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,39 +1,46 @@
 import unittest
 import requests
-import json
 
-data = {'email': 'admin@crm.com', 'password': '123456aB'}
+data_admin = {'email': 'admin@crm.com', 'password': '123456aB'}
+data_user = {'email': 'test@test.com', 'password': '123456aB'}
+data_customer = {'name': 'Testing', 'surname' : 'Testing'}
 
-access_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTYzMzM1OTg1MSwianRpIjoiYTZlNWZmMDktMWEwYS00N2ZlLTgwNDMtZTRmOGE1YWQ3YmJmIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6MSwibmJmIjoxNjMzMzU5ODUxLCJleHAiOjE2MzM0NDYyNTF9.32_UyALiSDZTWjHmxNhE_VmG1TB3rNX0t8scS2vWxAE'
-header = {'Content-type': 'application/json', 'Authorization' : 'Bearer {access_token}'}
+def get_token():
+    r = requests.post("http://127.0.0.1:5000/login", json = data_admin)
+    token = r.json()['token']
+    return(token)
+
+access_token = get_token()
+header = {'Content-type': 'application/json', 'Authorization' : f'Bearer {access_token}'}
 
 class ApiTest(unittest.TestCase):
+
     API_URL = "http://127.0.0.1:5000/"
     USER_URL = "{}user".format(API_URL)
     CUSTOMER_URL = "{}customer".format(API_URL)
     LOGIN = "{}login".format(API_URL)
 
     def test_0_login(self):
-        r = requests.post(ApiTest.LOGIN, json = data)
+        r = requests.post(ApiTest.LOGIN, json = data_admin)
         self.assertEqual(r.status_code, 200)
-        # This get the token string
-        print(r.json()['token'])
         
-    def test_1_testing(self):
-        r = requests.get(ApiTest.USER_URL, headers=header)
-        print(r.json())
+    def test_1_get_all_users(self):
+        r = requests.get(ApiTest.USER_URL, headers = header)
         self.assertEqual(r.status_code, 200)
 
-    def test_2_get_all_users(self):
-        r = requests.get(ApiTest.USER_URL, headers=header)
-        print(r.json())
-        self.assertEqual(r.status_code, 200)
-        self.assertEqual(len(r.json()), 2)
+    def test_3_post_customer(self):
+        r = requests.post(ApiTest.CUSTOMER_URL, json = data_customer, headers = header)
+        self.assertEqual(r.status_code, 201)
 
-    def test_3_get_all_customers(self):
-        r = requests.get(ApiTest.CUSTOMER_URL, headers=header)
+    # def test_4_delete_customer(self):
+    #     r = requests.delete(ApiTest.CUSTOMER_URL + '/' + (str(id)), headers = header)
+    #     print(r.json())
+    #     self.assertEqual(r.status_codes, 200)
+
+    def test_5_get_all_customers(self):
+        r = requests.get(ApiTest.CUSTOMER_URL, headers = header)
         self.assertEqual(r.status_code, 200)
-        self.assertEqual(len(r.json()), 2)
+
 
         
 # if __name__ == '__main__':
